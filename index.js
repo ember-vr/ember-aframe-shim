d088cd89dfe09698e5153a5562e5978246cb8f48
Kelly Selden <kellyselden@gmail.com>
2017-02-22 21:00:11 -0800

    calculate the import path

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 /* jshint node: true */
 'use strict';
 
+const path = require('path');
 const Funnel = require('broccoli-funnel');
+
+const packageName = 'aframe';
 
 module.exports = {
   name: 'ember-aframe-shim',
 
-  treeForVendor() {
-    return new Funnel('node_modules/aframe/dist', {
-      files: ['aframe-master.js'],
-      destDir: 'aframe'
-    });
-  },
-
   included() {
     this._super.included.apply(this, arguments);
 
-    this.import('vendor/aframe/aframe-master.js', {
+    let resolvedPath = require.resolve(packageName);
+    this.dirname = path.dirname(resolvedPath);
+    this.basename = path.basename(resolvedPath);
+
+    this.import(`vendor/${packageName}/${this.basename}`, {
       using: [
-        { transformation: 'amd', as: 'aframe' }
+        { transformation: 'amd', as: packageName }
       ]
+    });
+  },
+
+  treeForVendor() {
+    return new Funnel(this.dirname, {
+      files: [this.basename],
+      destDir: packageName
     });
   }
 };
