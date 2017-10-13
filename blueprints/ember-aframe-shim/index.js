902c38c453eb8292bbab386cfbf33b78e1392ba9
Kelly Selden <kellyselden@gmail.com>
2017-10-12 19:21:19 -0700

    get aframe version from package.json in blueprint

--- blueprints/ember-aframe-shim/index.js
+++ blueprints/ember-aframe-shim/index.js
@@ -1,8 +1,15 @@
 /* eslint-env node */
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
 module.exports = {
   normalizeEntityName() {},
 
   afterInstall() {
-    return this.addPackageToProject('aframe');
+    let pkg = JSON.parse(fs.readFileSync(path.join(__dirname, '../../package.json'), 'utf8'));
+
+    return this.addPackageToProject('aframe', pkg.devDependencies['aframe']);
   }
 };
