cff641a8940f5dca935574f22dc253098ad6e1de
Kelly Selden <kellyselden@gmail.com>
2017-12-15 11:34:31 -0800

    use pkg-conf to look up version

--- blueprints/ember-aframe-shim/index.js
+++ blueprints/ember-aframe-shim/index.js
@@ -1,15 +1,12 @@
 /* eslint-env node */
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
-
 module.exports = {
   normalizeEntityName() {},
 
   afterInstall() {
-    let pkg = JSON.parse(fs.readFileSync(path.join(__dirname, '../../package.json'), 'utf8'));
-
-    return this.addPackageToProject('aframe', pkg.devDependencies['aframe']);
+    return require('pkg-conf')('devDependencies').then(devDependencies => {
+      return this.addPackageToProject('aframe', devDependencies['aframe']);
+    });
   }
 };
