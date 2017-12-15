66df81ba01acb5698e549e36aa3b7aa5d901e8e1
Kelly Selden <kellyselden@gmail.com>
2017-12-15 11:41:51 -0800

    use require to read package.json

--- blueprints/ember-aframe-shim/index.js
+++ blueprints/ember-aframe-shim/index.js
@@ -1,14 +1,11 @@
 /* eslint-env node */
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
-
 module.exports = {
   normalizeEntityName() {},
 
   afterInstall() {
-    let pkg = JSON.parse(fs.readFileSync(path.join(__dirname, '../../package.json'), 'utf8'));
+    let pkg = require('../../package.json');
 
     return this.addPackageToProject('aframe', pkg.devDependencies['aframe']);
   }
