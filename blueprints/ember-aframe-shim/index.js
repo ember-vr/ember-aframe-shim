61a8ff4c2ea9e6ab56a6e6e9a09899632606a6a8
Kelly Selden <kellyselden@gmail.com>
2017-12-17 11:21:34 -0800

    Revert "use require to read package.json"
    This reverts commit 66df81ba01acb5698e549e36aa3b7aa5d901e8e1.

--- blueprints/ember-aframe-shim/index.js
+++ blueprints/ember-aframe-shim/index.js
@@ -1,11 +1,14 @@
 /* eslint-env node */
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
+
 module.exports = {
   normalizeEntityName() {},
 
   afterInstall() {
-    let pkg = require('../../package.json');
+    let pkg = JSON.parse(fs.readFileSync(path.join(__dirname, '../../package.json'), 'utf8'));
 
     return this.addPackageToProject('aframe', pkg.devDependencies['aframe']);
   }
