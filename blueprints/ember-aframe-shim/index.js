6b5b291e73c04e185984ee03dec41e4c2a119e36
Kelly Selden <kellyselden@gmail.com>
2019-12-15 18:43:09 +0000

    pkg-conf cwd __dirname

--- blueprints/ember-aframe-shim/index.js
+++ blueprints/ember-aframe-shim/index.js
@@ -4,7 +4,10 @@ module.exports = {
   normalizeEntityName() {},
 
   async afterInstall() {
-    let devDependencies = await require('pkg-conf')('devDependencies');
+    let devDependencies = await require('pkg-conf', {
+      // Why isn't this the default?
+      cwd: __dirname
+    })('devDependencies');
 
     return await this.addPackageToProject('aframe', devDependencies['aframe']);
   }
